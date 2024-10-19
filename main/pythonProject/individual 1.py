98d8908097683add96c268301e70f52e7fe91c5f
RomeInFlames <wew545454@gmail.com>
2024-10-19 11:47:31 +0300

    Update individual 1.py

--- main/pythonProject/individual 1.py
+++ main/pythonProject/individual 1.py
@@ -7,17 +7,17 @@ if __name__ == '__main__':
     x1 = int(input("Rolls price? "))
     x2 = int(input("Cans of paint price? "))
 
-    Sum = 8*x1 + 2*x2
-    if 0 <= Sum < 200:
-        print(Sum)
-    elif 200 <= Sum < 500:
-        print(0.97 * Sum)
-    elif 500 <= Sum < 800:
-        print(0.95 * Sum)
-    elif 800 <= Sum < 1000:
-        print(0.93 * Sum)
-    elif Sum >= 1000:
-        print(0.91 * Sum)
+    summa = 8*x1 + 2*x2
+    if 0 <= summa < 200:
+        print(summa)
+    elif 200 <= summa < 500:
+        print(0.97 * summa)
+    elif 500 <= summa < 800:
+        print(0.95 * summa)
+    elif 800 <= summa < 1000:
+        print(0.93 * summa)
+    elif summa >= 1000:
+        print(0.91 * summa)
     else:
         print("Illegal value of price!", file=sys.stderr)
         exit(1)
